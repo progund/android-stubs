9d7e883e3882d2211be10ce7ae453c7495d6240b
Henrik Sandklef <hesa@sandklef.com>
2018-11-13 23:06:34 +0100

    added d method with exception

--- android/util/Log.java
+++ android/util/Log.java
@@ -2,29 +2,34 @@ package android.util;
 
 public class Log {
 
-    public static int d (String tag, String msg) {
-	System.out.println("DEBUG: " + tag + " | " + msg);
-	return 0;
-    }
+  public static int d (String tag, String msg) {
+    System.out.println("DEBUG: " + tag + " | " + msg;
+    return 0;
+  }
 
-    public static int e (String tag, String msg) {
-	System.out.println("DEBUG: " + tag + " | " + msg);
-	return 0;
-    }
+  public static int d (String tag, String msg, Exception e) {
+    System.out.println("DEBUG: " + tag + " | " + msg + " | " + e.getMessage());
+    return 0;
+  }
 
-    public static int i (String tag, String msg) {
-	System.out.println("DEBUG: " + tag + " | " + msg);
-	return 0;
-    }
+  public static int e (String tag, String msg) {
+    System.out.println("DEBUG: " + tag + " | " + msg);
+    return 0;
+  }
 
-    public static int w (String tag, String msg) {
-	System.out.println("DEBUG: " + tag + " | " + msg);
-	return 0;
-    }
+  public static int i (String tag, String msg) {
+    System.out.println("DEBUG: " + tag + " | " + msg);
+    return 0;
+  }
 
-    public static int v (String tag, String msg) {
-	System.out.println("DEBUG: " + tag + " | " + msg);
-	return 0;
-    }
+  public static int w (String tag, String msg) {
+    System.out.println("DEBUG: " + tag + " | " + msg);
+    return 0;
+  }
+
+  public static int v (String tag, String msg) {
+    System.out.println("DEBUG: " + tag + " | " + msg);
+    return 0;
+  }
 
 }
