3c9ff0969aa1dfe8fc30f15235d2daaa7792b3a6
Henrik Sandklef <hesa@sandklef.com>
2018-11-13 23:07:08 +0100

    typo

--- android/util/Log.java
+++ android/util/Log.java
@@ -3,7 +3,7 @@ package android.util;
 public class Log {
 
   public static int d (String tag, String msg) {
-    System.out.println("DEBUG: " + tag + " | " + msg;
+    System.out.println("DEBUG: " + tag + " | " + msg);
     return 0;
   }
 
